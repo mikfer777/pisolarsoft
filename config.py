06e28c9eb9b8803e6b08b59e331bf6fcb13fa18a
Michel FERREOL <michel.ferreol@atos.net>
2020-05-16 19:35:44 +0200

    init project from picheckvoltage

---
+++ config.py
@@ -0,0 +1,123 @@
+"""
+" Edit below this line to fit your needs
+"""
+
+## Path of the scripts which will run by checkvoltage
+# Script to run when no battery is plugged (array with script then parameters)
+NOBAT_SCRIPT_PATH = ["/root/raspberrypi/movingraspi/MovingRaspiPlus/Server/movingraspi.sh", "stop"]
+# Script to run when battery voltage is dangerously low (array with script then
+#   parameters)
+DNGBAT_SCRIPT_PATH = ["/root/pcvscripts/batterymail.sh", "&"]
+# Script to run when battery voltage is low (array with script then parameters)
+KOBAT_SCRIPT_PATH = ["/root/raspberrypi/movingraspi/MovingRaspiPlus/Server/movingraspi.sh", "stop"]
+# Script to run when battery voltage is good (array with script then parameters)
+OKBAT_SCRIPT_PATH = ["/root/raspberrypi/movingraspi/MovingRaspiPlus/Server/movingraspi.sh", "start"]
+
+# GPIO (BOARD numbering scheme) pin for good voltage LED
+GOODVOLTPIN = 11
+# GPIO (BOARD numbering scheme) pin for low voltage LED
+LOWVOLTPIN = 12
+# GPIO (BOARD numbering scheme) pin for kill switch (LOW value to stop powering
+#   assembly) 
+KILLPIN = 13
+
+## Define voltages
+# Number of batteries in series
+BATNUMBER = 8.0
+#BATNUMBER = 3.0
+# Fully charged voltage (for a single battery); 
+#   i.e. 1.4V for a NiMH battery, or 4.2V for a LiPo battery
+FULLBATVOLT = 1.4
+#FULLBATVOLT = 4.2
+# Discharged voltage (for a single battery); 
+#   i.e. 1.05V for a NiMH battery, or 3.4V for a LiPo battery
+#   You should use a conservative value in order to avoid destructive
+#   discharging
+LOWBATVOLT = 1.05
+#LOWBATVOLT = 3.4
+# Dangerous voltage (for a single battery);
+#   i.e. 1.0V for a NiMH battery, or 3.2V for a LiPo battery
+#   You should really not go below this voltage.
+DNGBATVOLT = 1.0
+#DNGBATVOLT = 3.2
+
+# Value (in ohms) of the lower resistor from the voltage divider, connected to
+#   the ground line (1 if no voltage divider). Default value (3900) is for a 
+#   battery pack of 8 NiMH batteries, providing 11.2V max, stepped down to about
+#   3.2V max.
+LOWRESVAL = 3900
+# Value (in ohms) of the higher resistor from the voltage divider, connected to 
+#   the positive line (0 if no voltage divider). Default value (10000) is for a
+#   battery pack of 8 NiMH batteries, providing 11.2V max, stepped down to about
+#   3.2V max.
+HIGHRESVAL = 10000
+# Voltage value measured by the MCP3008 when batteries are fully charged
+# It should be near 3.3V due to Raspberry Pi GPIO compatibility)
+VHIGHBAT = (BATNUMBER*FULLBATVOLT)*(LOWRESVAL)/(LOWRESVAL+HIGHRESVAL)
+# Voltage value measured by the MCP3008 when batteries are discharged
+VLOWBAT = (BATNUMBER*LOWBATVOLT)*(LOWRESVAL)/(LOWRESVAL+HIGHRESVAL)
+# Voltage value measured by the MCP3008 when batteries voltage is dangerously
+#   low
+VDNGBAT = (BATNUMBER*DNGBATVOLT)*(LOWRESVAL)/(LOWRESVAL+HIGHRESVAL)
+
+# ADC voltage reference (3.3V for Raspberry Pi)
+ADCVREF = 3.3
+
+# Compensation due to the difference between ADC voltage reference and 
+#   max value for voltage through resistor divider
+#   i.e. : with the given resistances values, VREF is 3.3 V, and max voltage
+#   through resistor divider is about 3.14V, leading to about 4.8% deviation.
+# This compensation is used to correct computed battery voltage return by
+#   network queries
+VCOMP = 1+(ADCVREF-VHIGHBAT)/ADCVREF
+
+## Define expected ADC values
+# MCP23008 channel to use (from 0 to 7)
+ADCCHANNEL = 0
+# MCP23008 should return this value when batteries are fully charged
+#  * 3.3 is the reference voltage (got from Raspberry Pi's +3.3V power line)
+#  * 1024.0 is the number of possible values (MCP23008 is a 10 bit ADC)
+ADCHIGH = VHIGHBAT / (ADCVREF / 1024.0)
+# MCP23008 should return this value when batteries are discharged
+#  * 3.3 is the reference voltage (got from Raspberry Pi's +3.3V power line)
+#  * 1024.0 is the number of possible values (MCP23008 is a 10 bit ADC)
+ADCLOW = VLOWBAT / (ADCVREF / 1024.0)
+# MCP23008 should return this value when batteries atteigns dangerous voltage
+#  * 3.3 is the reference voltage (got from Raspberry Pi's +3.3V power line)
+#  * 1024.0 is the number of possible values (MCP23008 is a 10 bit ADC)
+ADCDNG = VDNGBAT / (ADCVREF / 1024.0)
+# MCP should return a value lower than this one when no battery is plugged.
+# You should not use 0 because value is floatting around 0 / 150 when nothing
+#  is plugged to a analog channel. 
+ADCUNP = 300 # No battery plugged
+# Maximal MCP return value to consider as "voltage bounce" when kill switch is
+#  activated and voltage rises again as current use is reduced. This avoid to
+#  switch from dangerous mode to low battery mode and back for a long time.
+ADCDNGBOUNCE = ADCDNG + 200
+
+# Refresh rate (ms)
+REFRESH_RATE = 1000
+
+# Hostname or IP address to listen for network queries. '' for all interfaces
+TCPHOST = ''
+# TCP port
+TCPPORT = 50007
+
+# Enable to possibility to push battery values to SIGFOX network
+#   http://www.sigfox.com/ for more information.
+SIGFOX_ENABLE = 0
+# SIGFOX device serial device
+SIGFOX_DEVICE = "/dev/ttyUSB0"
+# SIGFOX device baud rate
+SIGFOX_DEV_BR = 9600
+# SIGFOX device byte size (5 to 8)
+SIGFOX_DEV_BS = 8
+# SIGFOX device parity ("[N]one", "[E]ven", "[O]dd", "[M]ark", "[S]pace")
+SIGFOX_DEV_PAR = "N"
+# SIGFOX device stop bits (1, 1.5, 2)
+SIGFOX_DEV_SB = 1
+# SIGFOX push rate (ms, MUST be a multiple of REFRESH_RATE)
+SIGFOX_PUSH_RATE = 300000
+
+# Display some debug values when set to 1, and nothing when set to 0
+DEBUGMSG = 0
---
+++ main.py
@@ -0,0 +1,219 @@
+#!/usr/bin/python
+import RPi.GPIO as GPIO
+import time
+import array
+import os
+import signal
+import subprocess
+
+import threading
+import socket
+
+from config import *
+from mcp3008 import *
+
+if SIGFOX_ENABLE == 1:
+  try:
+    import serial
+  except ImportError:
+    print ("Unable to import PySerial")
+    exit (-1)
+
+
+STATUSNOBAT = 0
+STATUSDNGBAT = 0
+STATUSLOWBAT = 0
+STATUSGOODBAT = 0
+ret = 0
+
+### Functions definition
+
+def runSocketServer():
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    s.bind((TCPHOST, TCPPORT))
+    s.listen(1)
+    while True:
+        conn, addr = s.accept()
+        conn.sendall(networkData())
+        conn.close()
+
+# Called on process interruption. Set all pins to "Input" default mode.
+def endProcess(signalnum = None, handler = None):
+    GPIO.cleanup()
+    socketThread._Thread__stop()
+    if STATUSGOODBAT == 1:
+        try:
+            p = subprocess.Popen(NOBAT_SCRIPT_PATH, stdout=subprocess.PIPE)
+        except OSError as detail:
+            print ("Could not execute " + NOBAT_SCRIPT_PATH[0] + " ", detail)
+
+    if SIGFOX_ENABLE == 1:
+        sigfox.close()
+
+    exit(0)
+
+# Put pins to out mode and low state.
+def initPins():
+    GPIO.setup(GOODVOLTPIN, GPIO.OUT)
+    GPIO.setup(LOWVOLTPIN, GPIO.OUT)
+    GPIO.setup(KILLPIN, GPIO.OUT)
+    GPIO.output(GOODVOLTPIN, GPIO.LOW)
+    GPIO.output(LOWVOLTPIN, GPIO.LOW)
+    GPIO.output(KILLPIN, GPIO.LOW)
+
+# Build data send remotely (TCP or SIGFOX network)
+def networkData():
+    return (str(ADCLOW)+'|'
+       +str(ret)+'|'
+       +str(ADCHIGH)+'||'
+       +str(BATNUMBER*LOWBATVOLT)+'|'
+       +str((ret*(BATNUMBER*FULLBATVOLT)/1024)*VCOMP)+'|'
+       +str(BATNUMBER*FULLBATVOLT))
+
+
+def sigfoxData():
+    return str((ret*(BATNUMBER*FULLBATVOLT)/1024)*VCOMP)[:10]
+
+### Main part
+
+if DEBUGMSG == 1:
+    print("Batteries high voltage:       " + str(VHIGHBAT))
+    print("Batteries low voltage:        " + str(VLOWBAT))
+    print("Batteries dangerous voltage:  " + str(VDNGBAT))
+    print("ADC high voltage value:       " + str(ADCHIGH))
+    print("ADC low voltage value:        " + str(ADCLOW))
+    print("ADC dangerous voltage value:  " + str(ADCDNG))
+
+
+# Get current pid
+pid = os.getpid()
+
+# Save current pid for later use
+try:
+    fhandle = open('/var/run/picheckvoltage.pid', 'w')
+except IOError:
+    print ("Unable to write /var/run/picheckvoltage.pid")
+    exit(1)
+fhandle.write(str(pid))
+fhandle.close()
+
+# Prepare handlers for process exit
+signal.signal(signal.SIGTERM, endProcess)
+signal.signal(signal.SIGINT, endProcess)
+
+# Use Raspberry Pi board pin numbers
+GPIO.setmode(GPIO.BOARD)
+
+# Init output pins
+initPins()
+
+socketThread = threading.Thread(None, runSocketServer, None, (), {})
+socketThread.daemon = True
+socketThread.start()
+
+sigfox = None;
+sigfoxPush = 0;
+if SIGFOX_ENABLE == 1:
+    sigfox = serial.Serial(port=SIGFOX_DEVICE, 
+                           baudrate=SIGFOX_DEV_BR, 
+                           parity=SIGFOX_DEV_PAR,
+                           stopbits=SIGFOX_DEV_SB,
+                           bytesize=SIGFOX_DEV_BS)
+
+
+
+while True:
+    # Read ADC measure on channel ADCCHANNEL
+    ret = readadc(ADCCHANNEL, SPICLK, SPIMOSI, SPIMISO, SPICS)
+    ret2 = readadc(1, SPICLK, SPIMOSI, SPIMISO, SPICS)
+    if DEBUGMSG == 1:
+      print("ADC value channel0: " + str(ret) + " (" + str((3.3 / 1024.0) * ret) + " V)")
+      print("ADC value channel1: " + str(ret2))
+
+
+    if ret < ADCUNP:
+        # No battery plugged : we switch all LED off, and run NOBAT_SCRIPT_PATH
+        GPIO.output(GOODVOLTPIN, GPIO.LOW)
+        GPIO.output(LOWVOLTPIN, GPIO.LOW)
+        GPIO.output(KILLPIN, GPIO.LOW)
+        if STATUSNOBAT == 0:
+            STATUSNOBAT = 1
+            STATUSDNGBAT = 0
+            STATUSLOWBAT = 0
+            STATUSGOODBAT = 0
+            try:
+                p = subprocess.Popen(NOBAT_SCRIPT_PATH, stdout=subprocess.PIPE)
+            except OSError as detail:
+                print ("Could not execute " + NOBAT_SCRIPT_PATH[0] + " ", detail)
+
+    elif ret < ADCDNG:
+        # Dangerous battery voltage : we switch OK LED off, KO LED on,
+        #   and run DNGBAT_SCRIPT_PATH
+        GPIO.output(GOODVOLTPIN, GPIO.LOW)
+        GPIO.output(LOWVOLTPIN, GPIO.HIGH)
+        GPIO.output(KILLPIN, GPIO.LOW)
+        if STATUSDNGBAT == 0:
+            STATUSNOBAT = 0
+            STATUSDNGBAT = 1
+            STATUSLOWBAT = 0
+            STATUSGOODBAT = 0
+            try:
+                p = subprocess.Popen(DNGBAT_SCRIPT_PATH, stdout=subprocess.PIPE)
+            except OSError as detail:
+                print ("Could not execute " + DNGBAT_SCRIPT_PATH[0] + " ", detail)
+    
+    elif ret < ADCLOW:
+        # Test if we were previously in dangerous status and are actually
+        #   encountering a voltage bounce situation after kill switch
+        #   activation. 
+        if STATUSDNGBAT == 1 and ret > ADCDNGBOUNCE:
+            # Low battery voltage : we switch OK LED off, KO LED on, 
+            #   and run KOBAT_SCRIPT_PATH
+            GPIO.output(GOODVOLTPIN, GPIO.LOW)
+            GPIO.output(LOWVOLTPIN, GPIO.HIGH)
+            GPIO.output(KILLPIN, GPIO.HIGH)
+            if STATUSLOWBAT == 0:
+                STATUSNOBAT = 0
+                STATUSDNGBAT = 0 
+                STATUSLOWBAT = 1 
+                STATUSGOODBAT = 0
+                try:
+                    p = subprocess.Popen(KOBAT_SCRIPT_PATH, stdout=subprocess.PIPE)
+                except OSError as detail:
+                    print ("Could not execute " + KOBAT_SCRIPT_PATH[0] + " ", detail)
+
+    else:
+        # Normal battery voltage : we switch OK LED on, KO LED off, 
+        #   and OKBAT_SCRIPT_PATH
+        GPIO.output(GOODVOLTPIN, GPIO.HIGH)
+        GPIO.output(LOWVOLTPIN, GPIO.LOW)
+        GPIO.output(KILLPIN, GPIO.HIGH)
+        if STATUSGOODBAT == 0:
+            STATUSNOBAT = 0
+            STATUSDNGBAT = 0
+            STATUSLOWBAT = 0
+            STATUSGOODBAT = 1
+            try:
+                p = subprocess.Popen(OKBAT_SCRIPT_PATH, stdout=subprocess.PIPE)
+            except OSError as detail:
+                print ("Could not execute " + OKBAT_SCRIPT_PATH[0] + " ", detail)
+
+    # Push data to SIGFOX network
+    if SIGFOX_ENABLE == 1:
+        if sigfoxPush % (SIGFOX_PUSH_RATE / REFRESH_RATE) == 0:
+            import datetime
+            today = datetime.datetime.today()
+            sigfox.write("at$ss=" + sigfoxData().encode("hex") + "\r")
+            if DEBUGMSG == 1:
+                print today.strftime("%Y-%m-%d %H:%M:%S - Sent to SIGFOX: " 
+                                   + sigfoxData().encode("hex")
+                                   + " a.k.a " + sigfoxData() 
+                                   + "(V) in hexadecimal")
+        sigfoxPush = sigfoxPush + 1
+        # You don't want the counter to become too big :)
+        if sigfoxPush > (SIGFOX_PUSH_RATE / REFRESH_RATE):
+            sigfoxPush = 1
+
+    # Pause before starting loop once again
+    time.sleep(REFRESH_RATE / 1000)
